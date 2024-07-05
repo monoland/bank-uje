c133a0bb0345f2157045764e714ad15b355564d9
sudarmono <30992530+monoland@users.noreply.github.com>
2024-07-05 14:13:34 +0700

    update

--- public/build/assets/TestRoute-Do6G4h3K.js
+++
@@ -1,1 +0,0 @@
-import{am as e,as as t,ap as c}from"./app-BTse05vx.js";const o={};function r(s,a){return c(),t("h1",null,"I'm here to test!")}const _=e(o,[["render",r]]);export{_ as default};
---
+++ public/build/assets/index-CEnLDWFM.js
@@ -0,0 +1,1 @@
+import{a2 as e,ar as c,a7 as n}from"./app-BBF9j33d.js";const r={};function a(t,o){return n(),c("div",null,"x")}const _=e(r,[["render",a]]);export{_ as default};
